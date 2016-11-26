f1a6f48fb58500bed3123e6096b2736bac3a1b39
Joey <mf1633056@smail.nju.edu.cn>
2016-11-26 20:13:29 +0800

    fix some bugs

--- RPCtest_client.py
+++ RPCtest_client.py
@@ -19,44 +19,28 @@ def time_to_string(time_m):
 
 class MainWindow:
 
-    def buttonListener1(self, event):
-
-        def tick():
-            # get the current local time from the PC
-            time_g = s.getServerTime()
-            time_string = time_to_string(time_g)
-            # if time string has changed, update it
-            clock.config(text=time_string)
-            clock.after(200, tick)
-
-        root = Tk()
-        clock = Label(root, font=('times', 20, 'bold'), bg='green')
-        clock.grid(row=0, column=1)
-        tick()
-        root.mainloop()
-
-    def buttonListener2(self, event):
-        tkinter.messagebox.showinfo("messagebox", "this is button 2 dialog")
-
     def center_window(self, root, width, height):
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(size)
 
+    def tick(self):
+        # get the current local time from the PC
+        time_g = s.getServerTime()
+        time_string = time_to_string(time_g)
+        # if time string has changed, update it
+        self.clock.config(text=time_string)
+        self.clock.after(500, self.tick)
+
     def __init__(self):
         self.frame = Tk()
         self.frame.title('show server time')
-        self.center_window(self.frame, 300, 120)
+        self.center_window(self.frame, 300, 60)
 
-        self.button1 = Button(self.frame, text="digital mode", width=12, height=2)
-        self.button2 = Button(self.frame, text="analogue mode", width=20, height=2)
-
-        self.button1.pack(padx=20, side='left')
-        self.button2.pack(padx=10, side='right')
-
-        self.button1.bind("<ButtonRelease-1>", self.buttonListener1)
-        self.button2.bind("<ButtonRelease-1>", self.buttonListener2)
+        self.clock = Label(self.frame, font=('times', 20, 'bold'), bg='green')
+        self.clock.pack()
+        self.tick()
 
         self.frame.mainloop()
 
