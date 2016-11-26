c6c6c54d2f05a652d53a63c0311d948096e449d5
Joey <mf1633056@smail.nju.edu.cn>
2016-11-26 10:56:50 +0800

    complete the server-end code

--- RPCtest_server.py
+++ RPCtest_server.py
@@ -1,3 +1,4 @@
+import datetime
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
@@ -11,24 +12,13 @@ server = SimpleXMLRPCServer(("192.168.159.129", 8000),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
 
-# Register pow() function; this will use the value of
-# pow.__name__ as the name, which is just 'pow'.
-server.register_function(pow)
-
 
 # Register a function under a different name
-def adder_function(x,y):
-    return x + y
-server.register_function(adder_function, 'add')
+def get_server_time():
+    return datetime.datetime.now()
+server.register_function(get_server_time, 'getServerTime')
 
-
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'mul').
-class MyFuncs:
-    def mul(self, x, y):
-        return x * y
-
-server.register_instance(MyFuncs())
+print('server is ready for request!')
 
 # Run the server's main loop
 server.serve_forever()
